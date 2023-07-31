fb2161194495d37a880662673910040736a1f6d5
itivan <itivan@gmail.com>
2023-07-31 22:46:49 +0300

    f

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+
+    class Mate:
+        verbose_name = 'книга'
+        verbose_name_plural = 'книги'
+
+    title = models.CharField(max_length=100, verbose_name='название книги')
